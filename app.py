e089ec6fcd70f9f3d478c720229765f892bc5ada
Piyush <mesrivastava.piyush@gmail.com>
2024-03-18 20:43:08 +0530

    heroku4

--- app.py
+++ app.py
@@ -114,5 +114,6 @@ if __name__ == '__main__':
     with app.app_context():  # Ensure operations are within application context
         db.create_all()
         print("Database tables created successfully")
-    socketio.run(app)
+    app.run()
+    # socketio.run(app)
     # socketio.run(app,ssl_context=('cert.pem', 'key.pem'))
